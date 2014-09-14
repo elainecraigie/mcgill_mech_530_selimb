257965e2f1959552892f746da0e51e13a5c4ad77
Kreger51 <selim.belhaouane@gmail.com>
2014-09-13 20:36:52 -0400

    Automate PDF making process

---
+++ makeasspdf.bat
@@ -0,0 +1,23 @@
+@echo off
+::http://superuser.com/questions/246837/how-do-i-add-text-to-the-beginning-of-a-file-in-bash
+::START FROM mech_530
+::Use %1 to refer to command line argument (the html)
+::Use %~n1 to strip extension
+set str=%~n1
+echo %str%
+set assnum=%str:~-1%
+echo %assnum%
+ipython nbconvert --to html %1
+set notebookfile=notebook_%assnum%
+wkhtmltopdf --image-quality 300 %str%.html tex_pdf/%notebookfile%.pdf
+del %str%.html
+cd tex_pdf
+set /P thedate=Enter due date without year.
+set titlefile=titlepage_%assnum%
+PAUSE
+echo \def\assnum{%assnum%}\def\thedate{%thedate%, 2014} | cat - composites_template.tex > %titlefile%.tex
+PAUSE
+pdflatex %titlefile%.tex
+pdftk %titlefile%.pdf %notebookfile%.pdf cat output composites_1.pdf
+del %titlefile%*.*
+del %notebookfile%*.*
