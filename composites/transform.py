38de948322ab8226c5abcd8ec576b20addec5b6e
Kreger51 <selim.belhaouane@gmail.com>
2014-09-21 01:22:26 -0400

    Typo

--- composites/transform.py
+++ composites/transform.py
@@ -10,7 +10,7 @@ Arguments:
 import numpy
 from math import cos,sin,radians
 
-def transform_stress(values,input_axis,angle):
+def transform_stress(values,input_axis,angle, do_debug = False):
 	if input_axis == 'on':
 		angle = radians(-1*float(angle))
 	elif input_axis == 'off':
@@ -28,13 +28,16 @@ def transform_stress(values,input_axis,angle):
 	# b = numpy.array([p,q,r])
 	A = numpy.array([[p,q,r],[p,-q,-r],[0,r,-q]])
 	b = numpy.array([1,cos(2*angle),sin(2*angle)]) 
+	if do_debug:
+		print "p : %f" % p
+		print "q : %f" % q
+		print "r : %f" % r
+		print "A : ", A
+		print "b : ",b
 	ret_array = A.dot(b)
 	return ret_array
 
-if __name__ == '__main__':
-	sigma_on = transform([420,-165,-135],'off',30)
-
-def transform_strain(values,input_axis,angle):
+def transform_strain(values,input_axis,angle, do_debug = False):
 	if input_axis == 'on':
 		angle = radians(-1*float(angle))
 	elif input_axis == 'off':
@@ -44,7 +47,18 @@ def transform_strain(values,input_axis,angle):
 	p = float((values[0]+values[1])/2.0)
 	q = float((values[0]-values[1])/2.0)
 	r = float(values[2]/2.0)
+
 	A = numpy.array([[p,q,r],[p,-q,-r],[0, 2*r, -2*q]])
 	b = numpy.array([1, cos(2*angle), sin(2*angle)])
+	if do_debug:
+		print "p : %f" % p
+		print "q : %f" % q
+		print "r : %f" % r
+		print "A : ", A
+		print "b : ",b
+		
 	ret_array = A.dot(b)
 	return ret_array
+
+if __name__ == '__main__':
+	sigma_on = transform_stress([0.420,-0.165,-0.135],'off',30, do_debug = True)
